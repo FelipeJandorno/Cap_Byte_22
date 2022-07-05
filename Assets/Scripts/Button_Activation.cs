a5cfcdfe0302f220d8752d1ffac24e9530aab6a6
Felipe Figueiredo <84052051+FelipeJandorno@users.noreply.github.com>
2022-07-05 09:47:59 -0300

    Add files via upload

---
+++ Assets/Scripts/Button_Activation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Activation : MonoBehaviour {
+
+	public void Button_01(){
+		Debug.Log("botão 01");
+	}
+
+	public void Button_02(){
+		Debug.Log("botão 02");
+	}
+
+	public void Button_03(){
+		Debug.Log("botão 03");
+	}
+
+	public void Button_04(){
+		Debug.Log("botão 04");
+	}
+}
---
+++ Assets/Scripts/Change_Color.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change_Color : MonoBehaviour {
+
+	public Text Color_text;
+	public int code;
+	public float wait_time = 3f, rate = 2f;
+
+	void Start(){
+		InvokeRepeating("Random_Color", wait_time, rate);
+	}
+
+	void Update(){
+		if(code%2 == 0){
+			Color_text.text = "azul".ToString(); 
+		} else{
+			Color_text.text = "Verde".ToString();
+		}		
+	}
+
+	void Random_Color(){
+		code = Random.Range(1, 6);
+	}
+}
